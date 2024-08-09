3478adf5737b1c5cf9cae527415ef8471467bb37
Anton <allians19@gmail.com>
2024-08-09 14:17:44 +0500

    Соло на клавиатуре

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,16 +17,15 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keyup', e => {
+      if(e.key == this.currentSymbol.innerHTML) {
+        this.success()
+      } else {
+        this.fail()
+      }
+    })
   }
-
+  
   success() {
     if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
     this.currentSymbol.classList.add('symbol_correct');
@@ -56,6 +55,7 @@ class Game {
     const word = this.getWord();
 
     this.renderWord(word);
+    return word
   }
 
   getWord() {
